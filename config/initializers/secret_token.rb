9ae7fa51d3346ede2b5cad58b1e9fb2f9c49eb15
Celeste Layne <layne.celeste@gmail.com>
2015-02-01 15:29:37 -0800

    RailsBridge Job Board

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobBoard::Application.config.secret_key_base = '35139a97de1362768e5c3efad91e08953483427bf4183a9f02fea497a1dbb2d3f2b093aee632e79afb278766ff63bcb22391033b7e66ed50269b1626599b07cf'
